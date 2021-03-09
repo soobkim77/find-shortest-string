86f511333ab30cc95581deb2d50509f8b5ea42e3
Soo-Beom <soobeomkim77@gmail.com>
2021-03-09 13:43:11 -0800

    Completed Assignment

--- javascript/find_shortest_string.js
+++ javascript/find_shortest_string.js
@@ -1,5 +1,11 @@
 function findShortestString(arr) {
-  // type your code here
+  let x = arr[0];
+  arr.forEach(i =>{
+    if(i.length < x.length){
+      i=x
+    }
+  })
+  return x
 }
 
 if (require.main === module) {
@@ -18,7 +24,22 @@ if (require.main === module) {
   console.log("=>", findShortestString(['flower', 'juniper', 'lily', 'dadelion']));
 
   // BENCHMARK HERE
+  const startTime = Date.now();
+
+  for (let i = 0; i < 1000; ++i) {
+    findShortestString(['flower', 'juniper', 'lily', 'dadelion']);
+  }
+
+  const avgTime = (Date.now() - startTime) / 1000;
+  console.log(avgTime);
 }
+
+/*
+take a value from the index, can start at 0
+set comparison i < x
+if true, i = x
+iterate over each element
+*/ 
 
 module.exports = findShortestString;
 
